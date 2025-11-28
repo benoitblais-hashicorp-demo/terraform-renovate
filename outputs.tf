5cd8cad7220fb37d806b372e6e4bd31b1301d48b
Benoit Blais <benoit.blais@hashicorp.com>
2025-11-28 16:00:01 -0500

    initial release

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "uuid" {
+  description = "The generated uuid presented in string format."
+  value       = module.uuid.uuid
+}
---
+++ versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+  required_version = ">= 1.7.5"
+}
